d7e5276ee6a50894d8f7ba1a15c0a0687ce9131e
Alex <alexander.v.kazantsev@gmail.com>
2017-05-20 23:25:16 -0400

    I blame Max

--- pid.ino/pid/pid.ino
+++ pid.ino/pid/pid.ino
@@ -3,7 +3,6 @@
 #include "MsTimer2.h"
 
 
-
 /* ----- lead screw motor ------ */
 
 #define MOTOR_LS_FWD 5
@@ -15,8 +14,14 @@ void set_drive(double volts)
   if(volts < -12) volts = -12;
   uint8_t dc = (uint8_t)((abs(volts)*255)/12);
   if(volts > 0)
+  {
     analogWrite(MOTOR_LS_FWD, dc);
-  else analogWrite(MOTOR_LS_REV, dc);
+    analogWrite(MOTOR_LS_REV, 0);
+  }
+  else{ 
+    analogWrite(MOTOR_LS_REV, dc);
+    analogWrite(MOTOR_LS_FWD, 0);
+  }
 }
 
 
@@ -30,9 +35,9 @@ bool pa_onleft = true;
 void pluck()
 {
   if(pa_onleft) {
-    analogWrite(MOTOR_PA_FWD, 0xe0);
+    digitalWrite(MOTOR_PA_FWD, HIGH);
   } else {
-    analogWrite(MOTOR_PA_REV, 0xe0);
+    digitalWrite(MOTOR_PA_REV, HIGH);
   }
   MsTimer2::start();
 }
@@ -40,19 +45,11 @@ void pluck()
 void __epluck()
 {
   pa_onleft = !pa_onleft;
-  analogWrite(MOTOR_PA_FWD, 0x00);
-  analogWrite(MOTOR_PA_REV, 0x00);
+  digitalWrite(MOTOR_PA_FWD, LOW);
+  digitalWrite(MOTOR_PA_REV, LOW);
   MsTimer2::stop();
 }
 
-MIDI_CREATE_DEFAULT_INSTANCE();
-
-void midi_noteon(uint8_t chan, uint8_t pitch, uint8_t vel)
-{
-  // TODO: set the position
-  Serial.println("midi cmd");
-  pluck();
-}
 
 /* ----- i2c position feedback ----- */
 
@@ -81,20 +78,20 @@ int16_t zero_count()
 
 double input;
 double e = 0;
-double set = 0.2;
+double set_point = 0.15;
 double integral = 0;
 double prop = 0;
-double I = 7;
-double P = 80;
+double I = 70;
+double P = 180;
 
-#define CURPOS (((double)get_count()/600) * 0.008)
+#define CURPOS (((double)get_count()/360) * 0.008)
 #define dt (0.0026)
 
-double pid( double y, double r, double oldError)
+double pid(double y, double r, double oldError)
 {
   double error = r - y;
 
-  integral += I*(dt*(10*(error-oldError))) + dt*oldError;
+  integral += I*(dt*(10*(error-oldError)) + dt*oldError);
   prop = P*error;
 
   input = integral + prop;
@@ -128,57 +125,62 @@ void home()
   zero_count();
 }
 
+/* MIDI shit */
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+void midi_noteon(uint8_t chan, uint8_t pitch, uint8_t vel)
+{
+  // TODO: set the position
+  pluck();
+  set_point = (double)pitch/127 * 0.4;
+}
+
 void setup()
 {
   /* Setup MIDI */
-  MIDI.begin();
-  //MIDI.begin(MIDI_CHANNEL_OMNI);  // Listen to all incoming messages
-  MIDI.setHandleNoteOn(midi_noteon);
   Serial.begin(115200);
+  MIDI.begin(MIDI_CHANNEL_OMNI);  // Listen to all incoming messages
+  MIDI.setHandleNoteOn(midi_noteon);
 
-  //Serial.begin(9600);
   Serial.println("------------------------");
   
   /* output PWM (lead screw motor) */
   pinMode(MOTOR_LS_FWD, OUTPUT);
   pinMode(MOTOR_LS_REV, OUTPUT);
- 
+  //setPwmFrequency(9, 1);
+  //setPwmFrequency(10, 1);
+  TCCR0B = TCCR2B & 0b11111000 | 0x01; //  set PWM to 31k
   /* output PWM (plucking assy motor) */
   pinMode(MOTOR_PA_FWD, OUTPUT);
   pinMode(MOTOR_PA_REV, OUTPUT);
   MsTimer2::set(200, __epluck);
 
   /* I2C */
-  /*Wire.begin();
-  pinMode(SLAVE_RESET_PIN, OUTPUT);*/
+  Wire.begin();
+  pinMode(SLAVE_RESET_PIN, OUTPUT);
   
-  //home();
-  //double x = CURPOS;
+  home();
+  double x = CURPOS;
 }
 
 void loop()
 {
   // Read incoming messages
 
-/*
+
   double x = CURPOS;
 
-  e = pid( x, set, e );
+  e = pid( x, set_point, e );
   set_drive( input );
-  
+  /*
   Serial.print(x*1000);
   Serial.print("  ");
   Serial.print(e*1000);
   Serial.print("  ");
-  Serial.println(input);
-*/
+  Serial.println(input);*/
+
   
-  if(MIDI.read())
-  {
-    Serial.println("MIDI received");
-      delay(1000);
-      pluck();
-  }
+  MIDI.read();
 
 }
 
